510e428e2c55e50b92eb4a371b35266468091411
GabeValdivia <gabe@gabeswebcoding.com>
2021-09-19 17:43:17 -0500

    Add CSS modules for about and index pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import Layout from './components/Layout';
+import Layout from '../components/Layout';
+import {page, text} from '../examples/about.module.css'
 
 const about = () => {
 	return (
 		<Layout>
-			<h1>Hello from about page!</h1>
+			<div className={page}>
+				<h1>About Page</h1>
+				<h1>Welcome to Codefuzed!</h1>
+				<p className={text}>Diam ac dolor euismod tempus magna sollicitudin ipsum maximus nunc vivamus purus enim aliquam proin urna hendrerit commodo arcu lacus proin et arcu mi erat diam ex tincidunt consectetur consectetur lorem rutrum elit portaest eros congue nisl tempus massa vivamus mi ipsum condimentum sem ut.</p>
+			</div>
 		</Layout>
 	);
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import Layout from './components/Layout';
+import Layout from '../components/Layout';
+import {page, text} from '../examples/home.module.css'
 
 export default function Home() {
 	return (
 		<Layout>
-			<h1>Homepage</h1>
+			<div className={page}>
+				<h1>Homepage</h1>
+				<h1>Heading 2</h1>
+				<p>Consectetur varius varius urna eu arcu commodo orci elementum maximus ex eget nisi aliquam nunc pellentesque nisl cursus interdum sed lorem sem molestie vivamus euismod.</p>
+
+				<p className={text}>Varius suspendisse quis sed scelerisque lacus sollicitudin cursus hendrerit felis sollicitudin eros orci dolor nec lacus a portaest orci quam placerat dolor nec sit portaest.</p>
+			</div>
 		</Layout>
 	);
 }

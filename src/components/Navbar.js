e74c12e1a1e28f6e9f858ec65595bbe3ad9ec97b
GabeValdivia <gabe@gabeswebcoding.com>
2021-09-24 17:31:01 -0500

    Add gatsby-image-plugin. Add test image in /examples to test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Link } from 'gatsby';
+import {FiAlignJustify} from 'react-icons/fi';
+import logo from '../assets/images/logo.svg'
 
 const Navbar = () => {
+	const [show,setShow] = useState(false)
 	return (
-		<nav>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/about">About</Link>
-				</li>
-				<li>
-					<Link to="/tags">Tags</Link>
-				</li>
-				<li>
-					<Link to="/recipes">Recipes</Link>
-				</li>
-				<li>
-					<Link to="/contact">Contact</Link>
-				</li>
-			</ul>
+		<nav className="navbar">
+			<div className="nav-center">
+				<div className="nav-header">
+					<Link to="/">
+						<img src={logo} alt="recetas de mexico" />
+					</Link>
+					<button className="nav-btn" onClick={()=> setShow(!show)}>
+						<FiAlignJustify />
+					</button>
+				</div>
+				<div className={show ? "nav-links show-links":"nav-links"}>
+					<Link className="nav-link" activeClassName="active-link" to="/"
+					onClick={()=> setShow(false)}>home</Link>
+					<Link className="nav-link" activeClassName="active-link" to="/recipes" onClick={()=> setShow(false)}>recipes</Link>
+					<Link className="nav-link" activeClassName="active-link" to="/about" onClick={()=> setShow(false)}>about</Link>
+					<Link className="nav-link" activeClassName="active-link" to="/tags" onClick={()=> setShow(false)}>tags</Link>
+					<div className="nav-link contact-link">
+						<Link to="/contact" className="btn" onClick={()=> setShow(false)}>contact</Link>
+					</div>
+				</div>
+			</div>
 		</nav>
 	);
 };

ffe79ce4ceef587ed9502954ddb63ce84af35965
GabeValdivia <gabe@gabeswebcoding.com>
2021-10-03 18:48:30 -0500

    Set up siteMetaData in gatsby-config.js file

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,17 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata:{
+    title: 'Recetas Sencillas',
+    description: 'Recetas de comida Mexicana',
+    author: 'Gabriel Valdivia',
+    person: {name:"gabriel", age:48},
+    simpleData:['item 1','item 2'],
+    complexData:[
+      {name:"gabriel", age:48},
+      {name:"Jane", age:37},
+    ],
+  },
   plugins: [ 
   `gatsby-plugin-styled-components`,
   `gatsby-plugin-image`,
